de34fd28a0d483efb3d892a6f3d9676b699814a0
Teddy Li <teddyxlandlee@hotmail.com>
2026-07-01 01:21:49 +0800

    MixinOptions: modify param name

--- common/src/main/java/xland/mcmod/remoteresourcepack/mixin/MixinOptions.java
+++ common/src/main/java/xland/mcmod/remoteresourcepack/mixin/MixinOptions.java
@@ -11,7 +11,7 @@ import xland.mcmod.remoteresourcepack.RemoteResourcePack;
 @Mixin(Options.class)
 public abstract class MixinOptions {
     @Inject(method = "loadSelectedResourcePacks", at = @At("RETURN"))
-    private void afterLoadingPacks(PackRepository packRepository, CallbackInfo ci) {
-        RemoteResourcePack.insertEnabledPacks(packRepository);
+    private void afterLoadingPacks(PackRepository repository, CallbackInfo ci) {
+        RemoteResourcePack.insertEnabledPacks(repository);
     }
 }
